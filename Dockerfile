fbadb88b10e1fd7a67a62bf65476f2d61ef556be
mxBossard <mxbossard@gmail.com>
2022-07-26 20:34:51 +0200

    Add docker push.

--- Dockerfile
+++ Dockerfile
@@ -5,7 +5,8 @@ RUN apk add curl
 RUN curl https://github.com/ovh/venom/releases/download/v$VERSION/venom.linux-amd64 -L -o /usr/local/bin/venom && chmod +x /usr/local/bin/venom
 
 
-FROM busybox:1.34
+#FROM busybox:1.34
+FROM alpine:3.16
 
 RUN addgroup -g 1001 venom && adduser -D -u 1001 -G venom venom
 USER venom
--- build.sh
+++ build.sh
@@ -1,8 +1,12 @@
-#! /bin/sh
+#! /bin/sh -e
 scriptDir=$( readlink -f $( dirname $0 ) )
 
 VERSION=${1:-1.0.1}
+IMAGE_NAME=mxbossard/venom
 
 cd $scriptDir
-docker build -t venom:$VERSION --build-arg=VERSION .
+image=$IMAGE_NAME:$VERSION
+docker build -t $image --build-arg=VERSION .
+docker login
+docker push $image
 
